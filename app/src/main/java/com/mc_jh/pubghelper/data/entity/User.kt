5668c39ecd014ae536363d2f0144d8e92cb29ba1
junhwa lee <leejunhwa88@gmail.com>
2018-04-23 23:28:07 +0900

    구조 변경

---
+++ app/src/main/java/com/mc_jh/pubghelper/data/entity/User.kt
@@ -0,0 +1,6 @@
+package com.mc_jh.pubghelper.data.entity
+
+/**
+ * Created by Administrator on 2018-04-23.
+ */
+data class User(val name: String)
---
+++ app/src/main/java/com/mc_jh/pubghelper/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.mc_jh.pubghelper.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.mc_jh.pubghelper.R
+
+/**
+ * Created by Administrator on 2018-04-23.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
